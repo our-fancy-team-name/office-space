5bd6617aa21752f820987e0917d18e3b01d15e94
Dang Nguyen <nguyenmanhdang90@gmail.com>
2020-03-21 00:08:06 +0700

    sonarqube

--- office-space-postgres/src/main/java/com/ourfancyteamname/officespace/postgres/converters/RoleConverter.java
+++ office-space-postgres/src/main/java/com/ourfancyteamname/officespace/postgres/converters/RoleConverter.java
@@ -4,14 +4,11 @@ import com.ourfancyteamname.officespace.dtos.security.RoleDto;
 import com.ourfancyteamname.officespace.postgres.entities.Role;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
-import org.mapstruct.Mappings;
 
 @Mapper
 public interface RoleConverter {
 
-  @Mappings({
-      @Mapping(source = "role.code", target = "authority"),
-      @Mapping(expression = "java(currentlyUse.getId() == role.getId())", target = "isUsing")
-  })
+  @Mapping(source = "role.code", target = "authority")
+  @Mapping(expression = "java(currentlyUse.getId() == role.getId())", target = "isUsing")
   RoleDto toDto(Role role, Role currentlyUse);
 }
