e83c912041ff8e072547643322869d5e85f8833b
Dang Nguyen <nguyenmanhdang90@gmail.com>
2020-03-21 23:56:26 +0700

    fix deepscan

--- office-space-frontend/src/main/web/office-space-web/src/app/components/demo/demo.component.ts
+++ office-space-frontend/src/main/web/office-space-web/src/app/components/demo/demo.component.ts
@@ -1,6 +1,4 @@
-import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-demo',
