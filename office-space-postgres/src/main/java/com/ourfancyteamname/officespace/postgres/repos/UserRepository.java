cb3c140ce4595fc58b4896b6161705333bb87ce1
Dang Nguyen <nguyenmanhdang90@gmail.com>
2020-03-23 00:08:16 +0700

    add jpaSpecification

--- office-space-postgres/src/main/java/com/ourfancyteamname/officespace/postgres/repos/UserRepository.java
+++ office-space-postgres/src/main/java/com/ourfancyteamname/officespace/postgres/repos/UserRepository.java
@@ -2,12 +2,13 @@ package com.ourfancyteamname.officespace.postgres.repos;
 
 import com.ourfancyteamname.officespace.postgres.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
 @Repository
-public interface UserRepository extends JpaRepository<User, Integer> {
+public interface UserRepository extends JpaRepository<User, Integer>, JpaSpecificationExecutor<User> {
 
   Optional<User> findByUsername(String username);
 }
