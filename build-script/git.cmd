39a992de79b905ee98d086e63f04c13fefe7d015
dangnguyen <nguyenmanhdang90>
2020-12-30 16:01:30 +0700

    enable travis

--- build-script/git.cmd
+++ build-script/git.cmd
@@ -1,3 +1,4 @@
 git remote -v
 git remote set-url origin https://github.com/nguyen-manh-dang/office-space.git
 git remote set-url origin https://github.com/nguyenmanhdang90/office-space-v2.git
+git remote set-url origin https://github.com/our-fancy-team-name/office-space.git
