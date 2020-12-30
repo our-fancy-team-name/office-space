29a02d544f8d664c62377077c4c567999a7ebe08
dangnguyen <nguyenmanhdang90>
2020-12-30 14:18:31 +0700

    PermissionCodeTest

--- office-space-model/src/test/java/enums/PermissionCodeTest.java
+++ office-space-model/src/test/java/enums/PermissionCodeTest.java
@@ -1,12 +1,20 @@
 package enums;
 
 import com.ourfancyteamname.officespace.enums.PermissionCode;
+import org.apache.commons.lang3.ArrayUtils;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.util.stream.Stream;
+
 public class PermissionCodeTest {
+  public static final String[] names =
+      {"USER_EDIT", "ROLE_EDIT", "PRD_EDIT", "PKG_EDIT", "CLUS_EDIT", "NODE_EDIT", "PRCS_EDIT"};
+
   @Test
   public void name() {
-    Assert.assertEquals("USER_EDIT", PermissionCode.USER_EDIT.getName());
+    Stream.of(PermissionCode.values())
+        .map(PermissionCode::getName)
+        .forEach(s -> Assert.assertTrue(ArrayUtils.contains(names, s)));
   }
 }
