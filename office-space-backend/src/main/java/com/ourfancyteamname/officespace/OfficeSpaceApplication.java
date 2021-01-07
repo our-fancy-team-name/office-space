e8d50026580cf384e131ed1b47ffbc670294b644
dangnguyen <nguyenmanhdang90>
2021-01-07 16:40:19 +0700

    remove unused code

--- office-space-backend/src/main/java/com/ourfancyteamname/officespace/OfficeSpaceApplication.java
+++ office-space-backend/src/main/java/com/ourfancyteamname/officespace/OfficeSpaceApplication.java
@@ -1,12 +1,10 @@
 package com.ourfancyteamname.officespace;
 
 import com.ourfancyteamname.officespace.db.OfficeSpaceRepositoryApplication;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Import;
 
-@Slf4j
 @SpringBootApplication
 @Import(OfficeSpaceRepositoryApplication.class)
 public class OfficeSpaceApplication {
