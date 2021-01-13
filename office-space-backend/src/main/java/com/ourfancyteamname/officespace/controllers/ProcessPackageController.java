c5bdb6512ee43971cb966b8030d1ca47a30bcaee
dangnguyen <nguyenmanhdang90>
2021-01-13 08:49:23 +0700

    remove unused api

--- office-space-backend/src/main/java/com/ourfancyteamname/officespace/controllers/ProcessPackageController.java
+++ office-space-backend/src/main/java/com/ourfancyteamname/officespace/controllers/ProcessPackageController.java
@@ -2,10 +2,8 @@ package com.ourfancyteamname.officespace.controllers;
 
 import com.ourfancyteamname.officespace.db.entities.view.ProcessListView;
 import com.ourfancyteamname.officespace.dtos.ProcessPackageDto;
-import com.ourfancyteamname.officespace.dtos.TableSearchRequest;
 import com.ourfancyteamname.officespace.services.ProcessPackageService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -23,11 +21,6 @@ public class ProcessPackageController {
 
   @Autowired
   private ProcessPackageService processPackageService;
-
-  @PostMapping("/list-view")
-  public ResponseEntity<Page<ProcessListView>> getProcessListView(@RequestBody TableSearchRequest tableSearchRequest) {
-    return ResponseEntity.ok(null);
-  }
 
   @PostMapping("/add")
   @Transactional
