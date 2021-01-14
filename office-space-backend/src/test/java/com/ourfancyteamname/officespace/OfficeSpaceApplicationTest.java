bfe5ab114a2c93e70ca6318c7f3ea4ee171a77ea
dangnguyen <nguyenmanhdang90>
2021-01-14 20:50:16 +0700

    test 100%

--- office-space-backend/src/test/java/com/ourfancyteamname/officespace/OfficeSpaceApplicationTest.java
+++ office-space-backend/src/test/java/com/ourfancyteamname/officespace/OfficeSpaceApplicationTest.java
@@ -3,6 +3,7 @@ package com.ourfancyteamname.officespace;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
 import org.mockito.junit.MockitoJUnitRunner;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -10,10 +11,13 @@ import org.springframework.boot.test.context.SpringBootTest;
 @RunWith(MockitoJUnitRunner.class)
 public class OfficeSpaceApplicationTest {
 
+  @InjectMocks
+  private OfficeSpaceApplication application;
+
   @Test
   public void appClassTest() {
     Assert.assertEquals("public class com.ourfancyteamname.officespace.OfficeSpaceApplication",
-        OfficeSpaceApplication.class.toGenericString());
+        application.getClass().toGenericString());
   }
 
 }
