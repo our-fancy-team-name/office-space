0c763a903d22b148694bcdc8d32c914f6f216076
dangnguyen <nguyenmanhdang90>
2021-01-14 09:42:03 +0700

    reload permission when update cache

--- office-space-db/src/main/java/com/ourfancyteamname/officespace/db/repos/RolePermissionRepository.java
+++ office-space-db/src/main/java/com/ourfancyteamname/officespace/db/repos/RolePermissionRepository.java
@@ -11,18 +11,18 @@ import java.util.List;
 @Repository
 public interface RolePermissionRepository extends JpaRepository<RolePermission, Integer> {
 
-  @CacheEvict(cacheNames = CacheName.USER_PRINCIPLE, allEntries = true)
+  @CacheEvict(cacheNames = {CacheName.USER_PRINCIPLE, CacheName.PERMISSIONS}, allEntries = true)
   List<RolePermission> removeByRoleId(int roleId);
 
   @Override
-  @CacheEvict(cacheNames = CacheName.USER_PRINCIPLE, allEntries = true)
+  @CacheEvict(cacheNames = {CacheName.USER_PRINCIPLE, CacheName.PERMISSIONS}, allEntries = true)
   <S extends RolePermission> List<S> saveAll(Iterable<S> iterable);
 
   @Override
-  @CacheEvict(cacheNames = CacheName.USER_PRINCIPLE, allEntries = true)
+  @CacheEvict(cacheNames = {CacheName.USER_PRINCIPLE, CacheName.PERMISSIONS}, allEntries = true)
   <S extends RolePermission> S save(S s);
 
   @Override
-  @CacheEvict(cacheNames = CacheName.USER_PRINCIPLE, allEntries = true)
+  @CacheEvict(cacheNames = {CacheName.USER_PRINCIPLE, CacheName.PERMISSIONS}, allEntries = true)
   void deleteById(Integer integer);
 }
--- office-space-db/src/main/java/com/ourfancyteamname/officespace/db/repos/UserRoleRepository.java
+++ office-space-db/src/main/java/com/ourfancyteamname/officespace/db/repos/UserRoleRepository.java
@@ -1,6 +1,8 @@
 package com.ourfancyteamname.officespace.db.repos;
 
 import com.ourfancyteamname.officespace.db.entities.UserRole;
+import com.ourfancyteamname.officespace.enums.CacheName;
+import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,6 +10,14 @@ import java.util.List;
 
 @Repository
 public interface UserRoleRepository extends JpaRepository<UserRole, Integer> {
+
+  @CacheEvict(cacheNames = CacheName.USER_PRINCIPLE, allEntries = true)
   List<UserRole> removeByRoleId(int roleId);
+
+  @CacheEvict(cacheNames = CacheName.USER_PRINCIPLE, allEntries = true)
   List<UserRole> removeByUserId(int roleId);
+
+  @Override
+  @CacheEvict(cacheNames = CacheName.USER_PRINCIPLE, allEntries = true)
+  <S extends UserRole> List<S> saveAll(Iterable<S> iterable);
 }
