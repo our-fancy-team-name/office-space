64fc1d644c0dc04791270ab02fdb486218a97b8c
dangnguyen <nguyenmanhdang90>
2021-01-15 10:25:43 +0700

    fix duplicated code

--- office-space-db/src/main/java/com/ourfancyteamname/officespace/db/entities/ProcessCluster.java
+++ office-space-db/src/main/java/com/ourfancyteamname/officespace/db/entities/ProcessCluster.java
@@ -1,39 +1,19 @@
 package com.ourfancyteamname.officespace.db.entities;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
 
-import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
 import javax.persistence.Table;
-import java.io.Serializable;
 
 @Data
 @Entity
-@Builder
+@SuperBuilder
 @NoArgsConstructor
-@AllArgsConstructor
 @Table(name = "`process_cluster`")
-public class ProcessCluster implements Serializable {
+@EqualsAndHashCode(callSuper = true)
+public class ProcessCluster extends AbstractProcessObject {
   private static final long serialVersionUID = -1652730901265423406L;
-
-  @Id
-  @GeneratedValue(strategy = GenerationType.IDENTITY)
-  @Column(name = "`id`")
-  private int id;
-
-  @Column(name = "`name`")
-  private String name;
-
-  @Column(name = "`description`")
-  private String description;
-
-  @Column(name = "`code`")
-  private String code;
-
 }
--- office-space-db/src/main/java/com/ourfancyteamname/officespace/db/entities/ProcessNode.java
+++ office-space-db/src/main/java/com/ourfancyteamname/officespace/db/entities/ProcessNode.java
@@ -1,39 +1,19 @@
 package com.ourfancyteamname.officespace.db.entities;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
 
-import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
 import javax.persistence.Table;
-import java.io.Serializable;
 
 @Data
 @Entity
-@Builder
+@SuperBuilder
 @NoArgsConstructor
-@AllArgsConstructor
 @Table(name = "`process_node`")
-public class ProcessNode implements Serializable {
-  private static final long serialVersionUID = -1786540981265423406L;
-
-  @Id
-  @GeneratedValue(strategy = GenerationType.IDENTITY)
-  @Column(name = "`id`")
-  private int id;
-
-  @Column(name = "`name`")
-  private String name;
-
-  @Column(name = "`description`")
-  private String description;
-
-  @Column(name = "`code`")
-  private String code;
-
+@EqualsAndHashCode(callSuper = true)
+public class ProcessNode extends AbstractProcessObject {
+  private static final long serialVersionUID = -6241796290475282002L;
 }
