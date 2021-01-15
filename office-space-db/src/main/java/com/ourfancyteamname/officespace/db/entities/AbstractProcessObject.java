96b194530eb65bf3fd4354489c55df2e7f04a835
Dang Nguyen <nguyenmanhdang90@users.noreply.github.com>
2021-01-15 10:34:19 +0700

    fix duplicated code (#40)
    Co-authored-by: dangnguyen <nguyenmanhdang90>

---
+++ office-space-db/src/main/java/com/ourfancyteamname/officespace/db/entities/AbstractProcessObject.java
@@ -0,0 +1,37 @@
+package com.ourfancyteamname.officespace.db.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
+import java.io.Serializable;
+
+@Data
+@SuperBuilder
+@MappedSuperclass
+@NoArgsConstructor
+@AllArgsConstructor
+public abstract class AbstractProcessObject implements Serializable {
+
+  private static final long serialVersionUID = 41442188645158079L;
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Column(name = "`id`")
+  private int id;
+
+  @Column(name = "`name`")
+  private String name;
+
+  @Column(name = "`description`")
+  private String description;
+
+  @Column(name = "`code`")
+  private String code;
+}
